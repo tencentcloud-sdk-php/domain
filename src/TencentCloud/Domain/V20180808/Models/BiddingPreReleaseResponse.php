7d1c46366b333b8caeceeacfa3d070d8624cb7ff
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-28 04:41:15 +0800

    release 3.0.1369

--- src/TencentCloud/Domain/V20180808/Models/BiddingPreReleaseResponse.php
+++ src/TencentCloud/Domain/V20180808/Models/BiddingPreReleaseResponse.php
@@ -22,8 +22,8 @@ use TencentCloud\Common\AbstractModel;
  *
  * @method boolean getIsNeedPay() 获取是否需要额外支付
  * @method void setIsNeedPay(boolean $IsNeedPay) 设置是否需要额外支付
- * @method string getBillingParam() 获取计费请求参数，以Json字符串的形式进行返回。
- * @method void setBillingParam(string $BillingParam) 设置计费请求参数，以Json字符串的形式进行返回。
+ * @method string getBillingParam() 获取计费请求参数，以类Json字符串的形式进行返回。用于计费下单
+ * @method void setBillingParam(string $BillingParam) 设置计费请求参数，以类Json字符串的形式进行返回。用于计费下单
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
@@ -35,7 +35,7 @@ class BiddingPreReleaseResponse extends AbstractModel
     public $IsNeedPay;
 
     /**
-     * @var string 计费请求参数，以Json字符串的形式进行返回。
+     * @var string 计费请求参数，以类Json字符串的形式进行返回。用于计费下单
      */
     public $BillingParam;
 
@@ -46,7 +46,7 @@ class BiddingPreReleaseResponse extends AbstractModel
 
     /**
      * @param boolean $IsNeedPay 是否需要额外支付
-     * @param string $BillingParam 计费请求参数，以Json字符串的形式进行返回。
+     * @param string $BillingParam 计费请求参数，以类Json字符串的形式进行返回。用于计费下单
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
